fedd18969965788b7f26a49951a31877aa8e6ec0
Alexandru Radovici <msg4alex@gmail.com>
2016-08-22 08:18:58 +0000

    source

---
+++ source/bots/log.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var bus = require("./bus.js");
+
+bus.on ('message', function (type, id, message)
+{
+   console.log (type+' message from '+id+'-> '+message);
+});
---
+++ source/bots/trivia.js
@@ -0,0 +1,3 @@
+"use strict";
+
+var mesenger = require("./messenger.js");
