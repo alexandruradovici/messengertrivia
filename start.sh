ce80391603b73a1e9c81b11f0f098a6c8046c5d4
Alexandru Radovici <msg4alex@gmail.com>
2016-08-22 08:20:38 +0000

    without start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# this file should not be stored in the repository
-
-export MESSENGER_TOKEN=EAAHSMr0RQqYBAHvLRPZAjoGVo9AAJSqdre4O72Ojp7Qvlsh1pDrcFtJZBnKQ4YYPFn1tqrFnxnaD6byOeEN4oA8OsJTAaJnM47BvJfYaqXcq6JynRowShLTa0cHHNgbao64SP6Qo0rKPRcRlXSsJfx4cjZAYxMIwszojgqmXgZDZD
-node "source/index.js"
